4fbe55086ede47c8ca839c907335d52ea1f5406f
Alex <islicy@outlook.com>
2026-08-23 19:51:37 +0900

    Stabilize release byte normalization

--- lean4/verify_release.py
+++ lean4/verify_release.py
@@ -20,7 +20,7 @@ LIVE_PAPER = (
     / "paper"
     / "blackwell_theory_decision.tex"
 )
-EXPECTED_VERSION = "1.3.3"
+EXPECTED_VERSION = "1.3.4"
 
 
 def run(command: list[str], cwd: Path = LEAN_ROOT) -> str:
